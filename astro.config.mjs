c3d54f38f4872e6b2e795cb8821a77e859709d0f
Diego Lara Carvajal <dlaracarvajal00@mylangara.ca>
2024-01-08 16:28:29 -0800

    Image path updates v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: vercel()
 });
